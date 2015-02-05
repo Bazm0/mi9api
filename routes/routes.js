f29db67af130c069235a5acb0442f537778a50fc
Barry Quigley <barryquigley@yahoo.com>
2015-02-05 01:59:45 +0000

    logs update

--- routes/routes.js
+++ routes/routes.js
@@ -14,77 +14,78 @@ var routes = {
 
  res.header('Access-Control-Allow-Origin', '*');
  
-    try {
-        if (req.method !== 'POST') {
-            throw 'Invalid request method. Expected POST.';
-        }
-
-        data = JSON.parse(req.shows);
-
-        if (!data.payload || !data.payload.push) {
-            throw 'Request data is incorrectly formatted.';
-        }
-
-        res.json({
-            response:
-                data.payload
-                    .filter(function (payloadItem) {
-                        return payloadItem.image &&
-                                payloadItem.drm &&
-                                payloadItem.episodeCount > 0;
-                    })
-                    .map(function (payloadItem) {
-                        return { 
-                            image: payloadItem.image.showImage, 
-                            slug: payloadItem.slug, 
-                            title: payloadItem.title
-                        };
-                    })
-        });
-    }
-    catch (ex) {
-        res.json(400, { error: 'Could not decode request: ' + ex });
-    }
-
-
-
     // try {
-
-    //   res.header('Access-Control-Allow-Origin', '*');
-    //   // check request method 
-    //   if (req.method !== 'POST') {
-    //     throw new Error('Invalid request: ' + req.method);
-    //   }
-
-    //   var shows = JSON.parse(req.shows);
-
-    //   if (!shows.payload || !_.isArray(shows.payload)) {
-    //     throw new Error('Invalid post data.');
-    //   }
-
-    //   var result = _.reduce(shows.payload, function(result, item) {
-    //     if (item.drm === true && parseInt(item.episodeCount) > 0) {
-    //       logger.debug('item: ', item);
-    //       result.push({
-    //         image: item.image.showImage,
-    //         slug: item.slug,
-    //         title: item.title
-    //       });
+    //     if (req.method !== 'POST') {
+    //         throw 'Invalid request method. Expected POST.';
     //     }
-    //     return result;
-    //   }, []);
 
+    //     data = JSON.parse(req.shows);
 
-    //   res.status(200).send(result);
+    //     if (!data.payload || !data.payload.push) {
+    //         throw 'Request data is incorrectly formatted.';
+    //     }
 
-
-    // } catch (e) {
-
-    //   return res.status(400).send({
-    //     'error': 'Could not decode request: ' + e.message
-    //   });
-
+    //     res.json({
+    //         response:
+    //             data.payload
+    //                 .filter(function (payloadItem) {
+    //                     return payloadItem.image &&
+    //                             payloadItem.drm &&
+    //                             payloadItem.episodeCount > 0;
+    //                 })
+    //                 .map(function (payloadItem) {
+    //                     return { 
+    //                         image: payloadItem.image.showImage, 
+    //                         slug: payloadItem.slug, 
+    //                         title: payloadItem.title
+    //                     };
+    //                 })
+    //     });
     // }
+    // catch (ex) {
+    //     res.json(400, { error: 'Could not decode request: ' + ex });
+    // }
+
+    var shows;
+
+    res.header('Access-Control-Allow-Origin', '*');
+    
+    try {
+
+      // check request method 
+      if (req.method !== 'POST') {
+        throw new Error('Invalid request: ' + req.method);
+      }
+
+      shows = JSON.parse(req.shows);
+
+      if (!shows.payload || !_.isArray(shows.payload)) {
+        throw new Error('Invalid post data.');
+      }
+
+      var result = _.reduce(shows.payload, function(result, item) {
+        if (item.drm === true && parseInt(item.episodeCount) > 0) {
+          logger.debug('item: ', item);
+          result.push({
+            image: item.image.showImage,
+            slug: item.slug,
+            title: item.title
+          });
+        }
+        return result;
+      }, []);
+
+
+      res.status(200).send(result);
+
+
+    } catch (e) {
+
+      return res.status(400).send({
+        'error': 'Could not decode request: ' + e.message
+      });
+
+    }
 
 
   }
